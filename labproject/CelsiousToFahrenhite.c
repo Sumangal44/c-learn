6a97eb166ae2369a5e8f1c2d0d54919933a3561a
Sumangal44 <123644743+Sumangal44@users.noreply.github.com>
2024-07-28 19:06:00 +0530

    Update CelsiousToFahrenhite.c

--- labproject/CelsiousToFahrenhite.c
+++ labproject/CelsiousToFahrenhite.c
@@ -1,12 +1,12 @@
-// #include <stdio.h>
-// #include <stdlib.h>
+ #include <stdio.h>
+ #include <stdlib.h>
 
-// void main() {
-//   system("clear");
-//   float C, F;
-//   printf("Enter Celsius temperature::  ");
-//   scanf("%f", &C);
-//   F = (9*C/5)+32;
-//   printf("Your Fahrenhite  value is = %f\n", F);
-//   // getch();
-// }
+ void main() {
+   system("clear");
+   float C, F;
+   printf("Enter Celsius temperature::  ");
+  scanf("%f", &C);
+   F = (9*C/5)+32;
+   printf("Your Fahrenhite  value is = %f\n", F);
+   getch();
+ }
